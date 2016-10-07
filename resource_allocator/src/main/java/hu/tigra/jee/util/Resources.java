ca97b3e1c6320aa9b4477ff8540fb74bee167cc3
Hdavid24 <horvathdavid@vipmail.hu>
2016-10-07 13:30:09 +0200

    proba2

--- resource_allocator/src/main/java/hu/tigra/jee/util/Resources.java
+++ resource_allocator/src/main/java/hu/tigra/jee/util/Resources.java
@@ -26,11 +26,11 @@ import java.util.logging.Logger;
 
 /**
  * This class uses CDI to alias Java EE resources, such as the persistence context, to CDI beans
- * 
+ * <p>
  * <p>
  * Example injection on a managed bean field:
  * </p>
- * 
+ * <p>
  * <pre>
  * &#064;Inject
  * private EntityManager em;
